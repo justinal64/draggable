19547785eb29f0fbbbf4e660ceb9d81891dff8e8
justin leggett <justin@codingrookie.com>
2016-10-24 09:32:26 -0500

    added draggable canvas objects to the board

--- javascript/main.js
+++ javascript/main.js
@@ -21,10 +21,9 @@ $('.container').click((event)=> {
 
 function white() {
     // This is where I will build the row starting with a white block
-    currentLine = "";
+        currentLine = "";
     currentLine += `<div class="row">`;
-        currentLine += `<div class="col-xs-1 col-sm-1 col-md-1 col-md-offset-1 white"></div>`;
-            currentLine += `<div class="col-xs-1 col-sm-1 col-md-1 black"></div>`;
+        currentLine += `<div class="col-xs-1 col-sm-1 col-md-1 col-md-offset-2 black"></div>`;
             currentLine += `<div class="col-xs-1 col-sm-1 col-md-1 white"></div>`;
             currentLine += `<div class="col-xs-1 col-sm-1 col-md-1 black"></div>`;
             currentLine += `<div class="col-xs-1 col-sm-1 col-md-1 white"></div>`;
@@ -32,7 +31,36 @@ function white() {
             currentLine += `<div class="col-xs-1 col-sm-1 col-md-1 white"></div>`;
             currentLine += `<div class="col-xs-1 col-sm-1 col-md-1 black"></div>`;
             currentLine += `<div class="col-xs-1 col-sm-1 col-md-1 white"></div>`;
-            currentLine += `<div class="col-xs-1 col-sm-1 col-md-1 black"></div>`;
+    currentLine += `</div>`;
+    currentLine += `<div class="row">`;
+        currentLine += `<div class="col-xs-1 col-sm-1 col-md-1 col-md-offset-2 white"></div>`;
+            currentLine += `<div class="col-xs-1 col-sm-1 col-md-1 black"><canvas id="myCanvas_12" width="50px" height="50px"></canvas></div>`;
+            currentLine += `<div class="col-xs-1 col-sm-1 col-md-1 white"></div>`;
+            currentLine += `<div class="col-xs-1 col-sm-1 col-md-1 black"><canvas id="myCanvas_13" width="50px" height="50px"></canvas></div>`;
+            currentLine += `<div class="col-xs-1 col-sm-1 col-md-1 white"></div>`;
+            currentLine += `<div class="col-xs-1 col-sm-1 col-md-1 black"><canvas id="myCanvas_14" width="50px" height="50px"></canvas></div>`;
+            currentLine += `<div class="col-xs-1 col-sm-1 col-md-1 white"></div>`;
+            currentLine += `<div class="col-xs-1 col-sm-1 col-md-1 black"><canvas id="myCanvas_15" width="50px" height="50px"></canvas></div>`;
+    currentLine += `</div>`;
+    currentLine += `<div class="row">`;
+        currentLine += `<div class="col-xs-1 col-sm-1 col-md-1 col-md-offset-2 black"><canvas id="myCanvas_16" width="50px" height="50px"></canvas></div>`;
+            currentLine += `<div class="col-xs-1 col-sm-1 col-md-1 white"></div>`;
+            currentLine += `<div class="col-xs-1 col-sm-1 col-md-1 black"><canvas id="myCanvas_17" width="50px" height="50px"></canvas></div>`;
+            currentLine += `<div class="col-xs-1 col-sm-1 col-md-1 white"></div>`;
+            currentLine += `<div class="col-xs-1 col-sm-1 col-md-1 black"><canvas id="myCanvas_18" width="50px" height="50px"></canvas></div>`;
+            currentLine += `<div class="col-xs-1 col-sm-1 col-md-1 white"></div>`;
+            currentLine += `<div class="col-xs-1 col-sm-1 col-md-1 black"><canvas id="myCanvas_19" width="50px" height="50px"></canvas></div>`;
+            currentLine += `<div class="col-xs-1 col-sm-1 col-md-1 white"></div>`;
+    currentLine += `</div>`;
+    currentLine += `<div class="row">`;
+        currentLine += `<div class="col-xs-1 col-sm-1 col-md-1 col-md-offset-2 white"></div>`;
+            currentLine += `<div class="col-xs-1 col-sm-1 col-md-1 black"><canvas id="myCanvas_20" width="50px" height="50px"></canvas></div>`;
+            currentLine += `<div class="col-xs-1 col-sm-1 col-md-1 white"></div>`;
+            currentLine += `<div class="col-xs-1 col-sm-1 col-md-1 black"><canvas id="myCanvas_21" width="50px" height="50px"></canvas></div>`;
+            currentLine += `<div class="col-xs-1 col-sm-1 col-md-1 white"></div>`;
+            currentLine += `<div class="col-xs-1 col-sm-1 col-md-1 black"><canvas id="myCanvas_22" width="50px" height="50px"></canvas></div>`;
+            currentLine += `<div class="col-xs-1 col-sm-1 col-md-1 white"></div>`;
+            currentLine += `<div class="col-xs-1 col-sm-1 col-md-1 black"><canvas id="myCanvas_23" width="50px" height="50px"></canvas></div>`;
     currentLine += `</div>`;
     $board.append(currentLine);
 }
@@ -41,8 +69,37 @@ function black() {
     // This is where I will build the row starting with a black block
     currentLine = "";
     currentLine += `<div class="row">`;
-        currentLine += `<div class="col-xs-1 col-sm-1 col-md-1 col-md-offset-1 black"></div>`;
+        currentLine += `<div class="col-xs-1 col-sm-1 col-md-1 col-md-offset-2 black"><canvas id="myCanvas_0" width="50px" height="50px"></canvas></div>`;
             currentLine += `<div class="col-xs-1 col-sm-1 col-md-1 white"></div>`;
+            currentLine += `<div class="col-xs-1 col-sm-1 col-md-1 black"><canvas id="myCanvas_1" width="50px" height="50px"></canvas></div>`;
+            currentLine += `<div class="col-xs-1 col-sm-1 col-md-1 white"></div>`;
+            currentLine += `<div class="col-xs-1 col-sm-1 col-md-1 black"><canvas id="myCanvas_2" width="50px" height="50px"></canvas></div>`;
+            currentLine += `<div class="col-xs-1 col-sm-1 col-md-1 white"></div>`;
+            currentLine += `<div class="col-xs-1 col-sm-1 col-md-1 black"><canvas id="myCanvas_3" width="50px" height="50px"></canvas></div>`;
+            currentLine += `<div class="col-xs-1 col-sm-1 col-md-1 white"></div>`;
+    currentLine += `</div>`;
+    currentLine += `<div class="row">`;
+        currentLine += `<div class="col-xs-1 col-sm-1 col-md-1 col-md-offset-2 white"></div>`;
+            currentLine += `<div class="col-xs-1 col-sm-1 col-md-1 black"><canvas id="myCanvas_4" width="50px" height="50px"></canvas></div>`;
+            currentLine += `<div class="col-xs-1 col-sm-1 col-md-1 white"></div>`;
+            currentLine += `<div class="col-xs-1 col-sm-1 col-md-1 black"><canvas id="myCanvas_5" width="50px" height="50px"></canvas></div>`;
+            currentLine += `<div class="col-xs-1 col-sm-1 col-md-1 white"></div>`;
+            currentLine += `<div class="col-xs-1 col-sm-1 col-md-1 black"><canvas id="myCanvas_6" width="50px" height="50px"></canvas></div>`;
+            currentLine += `<div class="col-xs-1 col-sm-1 col-md-1 white"></div>`;
+            currentLine += `<div class="col-xs-1 col-sm-1 col-md-1 black"><canvas id="myCanvas_7" width="50px" height="50px"></canvas></div>`;
+    currentLine += `</div>`;
+    currentLine += `<div class="row">`;
+        currentLine += `<div class="col-xs-1 col-sm-1 col-md-1 col-md-offset-2 black"><canvas id="myCanvas_8" width="50px" height="50px"></canvas></div>`;
+            currentLine += `<div class="col-xs-1 col-sm-1 col-md-1 white"></div>`;
+            currentLine += `<div class="col-xs-1 col-sm-1 col-md-1 black"><canvas id="myCanvas_9" width="50px" height="50px"></canvas></div>`;
+            currentLine += `<div class="col-xs-1 col-sm-1 col-md-1 white"></div>`;
+            currentLine += `<div class="col-xs-1 col-sm-1 col-md-1 black"><canvas id="myCanvas_10" width="50px" height="50px"></canvas></div>`;
+            currentLine += `<div class="col-xs-1 col-sm-1 col-md-1 white"></div>`;
+            currentLine += `<div class="col-xs-1 col-sm-1 col-md-1 black"><canvas id="myCanvas_11" width="50px" height="50px"></canvas></div>`;
+            currentLine += `<div class="col-xs-1 col-sm-1 col-md-1 white"></div>`;
+    currentLine += `</div>`;
+    currentLine += `<div class="row">`;
+        currentLine += `<div class="col-xs-1 col-sm-1 col-md-1 col-md-offset-2 white"></div>`;
             currentLine += `<div class="col-xs-1 col-sm-1 col-md-1 black"></div>`;
             currentLine += `<div class="col-xs-1 col-sm-1 col-md-1 white"></div>`;
             currentLine += `<div class="col-xs-1 col-sm-1 col-md-1 black"></div>`;
@@ -50,23 +107,20 @@ function black() {
             currentLine += `<div class="col-xs-1 col-sm-1 col-md-1 black"></div>`;
             currentLine += `<div class="col-xs-1 col-sm-1 col-md-1 white"></div>`;
             currentLine += `<div class="col-xs-1 col-sm-1 col-md-1 black"></div>`;
-            currentLine += `<div class="col-xs-1 col-sm-1 col-md-1 white"></div>`;
     currentLine += `</div>`;
     $board.append(currentLine);
 }
 
-
-
 function whichColor() {
-    for(var i = 0; i < 10; i++) {
-        if(i % 2 === 0) {
+    // for(var i = 0; i < 5; i++) {
+        // if(i % 2 === 0) {
             black();
-            console.log("It's blacks turn");
-        } else {
             white();
-            console.log("It's whites turn");
-        }
-    }
+        // }
+        //  else {
+        //     white();
+        // }
+    // }
 }
 
 whichColor();
@@ -74,3 +128,53 @@ whichColor();
 $(() => {
     $( "#draggable" ).draggable();
 });
+
+$(() => {
+    $( "#myCanvas_4" ).draggable();
+});
+
+// Black Canvas Elements
+for(let i = 0; i <= 11; i++) {
+    var canvas = document.getElementById(`myCanvas_${i}`);
+    var context = canvas.getContext('2d');
+    var centerX = canvas.width / 2;
+    var centerY = canvas.height / 2;
+    var radius = 20;
+
+
+    context.beginPath();
+    context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
+    context.fillStyle = 'white';
+    context.fill();
+    context.lineWidth = 5;
+    context.strokeStyle = 'grey';
+    context.stroke();
+
+    // eventListener
+    $(() => {
+        $( `#myCanvas_${i}` ).draggable();
+    });
+}
+
+// Black Canvas Elements
+for(let i = 12; i <= 23; i++) {
+    var canvas = document.getElementById(`myCanvas_${i}`);
+    var context = canvas.getContext('2d');
+    var centerX = canvas.width / 2;
+    var centerY = canvas.height / 2;
+    var radius = 20;
+
+
+    context.beginPath();
+    context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
+    context.fillStyle = 'white';
+    context.fill();
+    context.lineWidth = 5;
+    context.strokeStyle = 'red';
+    context.stroke();
+
+    // eventListener
+    $(() => {
+        $( `#myCanvas_${i}` ).draggable();
+    });
+}
